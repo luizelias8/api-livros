0082e9db863b9cfb9c6073ebcff15e6fff6cd979
luizelias8 <luizelias8@gmail.com>
2025-02-02 07:18:23 -0300

    Cria teste unitário e atualiza configuração do CI para rodar testes pytest

---
+++ test_app.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from app import app
+
+cliente = TestClient(app)
+
+def test_rota_livros():
+    resposta = cliente.get('/livros')
+    assert resposta.status_code == 200
+
+def test_rota_autores():
+    resposta = cliente.get('/autores')
+    assert resposta.status_code == 200
+
+def test_rota_categorias():
+    resposta = cliente.get('/categorias')
+    assert resposta.status_code == 200
